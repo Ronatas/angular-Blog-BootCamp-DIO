07a43c29c80c09927972039fe3b24eeda1268966
Ronatas Diniz <ronatas.diniz@gmail.com>
2024-02-23 18:08:29 -0300

    Components dinamicos criados

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
-export class BigCardComponent {
-
+export class BigCardComponent implements OnInit {
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
+  @Input()
+  cardLink: string = '';
+  @Input()
+  cardFonte: string = '';
+  constructor() {}
+  ngOnInit(): void {}
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
   imports: [],
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
-export class SmallCardComponent {
-
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
+  @Input()
+  cardLink: string = '';
+  @Input()
+  cardFonte:string='';
+  constructor() {}
+  ngOnInit(): void {}
 }
